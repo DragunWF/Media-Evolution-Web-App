bf1632626cae4165244c0e280b5e8f48726006dd
DragonWF <plarisanmarc@gmail.com>
2022-02-20 18:56:02 +0800

    Added mentions for icon clicks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
-    
-})
+  let iconClicks = 0;
+  $(".icons").click(() => {
+    iconClicks++;
+    const times = iconClicks > 1 ? "times" : "time";
+    alert(
+      `Oh hey, someone clicked an icon. (You clicked this ${iconClicks} ${times})`
+    );
+  });
+});
