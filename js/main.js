f50d2bcca13c28c0a3ee614f236384f48e498253
DragonWF <plarisanmarc@gmail.com>
2022-02-21 18:55:24 +0800

    Added code for feedback form

--- js/main.js
+++ js/main.js
@@ -7,4 +7,25 @@ $(document).ready(() => {
       `Oh hey, someone clicked an icon. (You clicked this ${iconClicks} ${times})`
     );
   });
+
+  // Form
+  $("#form-submit").click(() => {
+    const name = $("#form-name").val();
+    const message = $("#form-textarea").val();
+    if (message) {
+      if (!name) $("#form-name").val("Anonymous");
+      if (name.length < 20 && message.length < 2000)
+        $("#form-submit").attr("type", "submit");
+      else {
+        if (name.length > 20) alert("Name must be less than 20 characters");
+        if (message.length > 2000)
+          alert("Message must be less than 2000 characters");
+      }
+    } else alert("Your message is empty!");
+  });
+
+  $("#form-reset").click(() => {
+    $("#form-name").val("");
+    $("#form-textarea").val("");
+  });
 });
